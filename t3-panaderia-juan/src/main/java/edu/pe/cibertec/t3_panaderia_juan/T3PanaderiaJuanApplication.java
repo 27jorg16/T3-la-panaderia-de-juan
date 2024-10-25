f02d357cc28785880c1f211d18719e16117c3b25
Juan Carlos <144704177+JuanCarlos1404@users.noreply.github.com>
2024-10-24 21:12:23 -0500

    Add files via upload

---
+++ t3-panaderia-juan/src/main/java/edu/pe/cibertec/t3_panaderia_juan/T3PanaderiaJuanApplication.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.t3_panaderia_juan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T3PanaderiaJuanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T3PanaderiaJuanApplication.class, args);
+	}
+
+}
---
+++ t3-panaderia-juan/src/test/java/edu/pe/cibertec/t3_panaderia_juan/T3PanaderiaJuanApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.t3_panaderia_juan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T3PanaderiaJuanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
